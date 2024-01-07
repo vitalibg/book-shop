4a05b659d451670f9e87ad8dc93302f65a0f2f19
Vitali Akulau <vitalibg2023@gmail.com>
2024-01-07 22:59:17 +0300

    fix new releases page [draft]

--- my-app/src/component/page/new-releases/new-releases-page.component.tsx
+++ my-app/src/component/page/new-releases/new-releases-page.component.tsx
@@ -3,14 +3,24 @@ import TextComponent from "../../common/text.component";
 import BookContainerComponent from "../../book/book-container/book-container.component";
 import PaginationComponent from "../../pagination/pagination.component";
 import SubscribeComponent from "../../subscribe/subscribe.component";
+import style from "./new-releases-page.module.css";
+import image from "./../../../assets/opened-book-06-adding-another-outline-850x638.png"
 
 const NewReleasesPageComponent = () => {
   return (
-    <div>
-      <TextComponent text={"New releases book"}/>
-      <BookContainerComponent image={"123"} title={"TestPrice"} description={"TestDescription"} price={"Price"} />
-      <PaginationComponent />
-      <SubscribeComponent />
+    <div className={style.container}>
+      <div className={style.title}>
+        <TextComponent text={"New releases book"} />
+      </div>
+      <div className={style.book}>
+        <BookContainerComponent image={image} title={"Title"} description={"TestDescription"} price={"123$"} />
+      </div>
+      <div className={style.pagination}>
+        <PaginationComponent />
+      </div>
+      <div className={style.subscribe}>
+        <SubscribeComponent />
+      </div>
     </div>
   );
 };
