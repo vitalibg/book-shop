1c984f7c3de0791cc2faa087f6bb61ef15caccd7
Vitali Akulau <vitalibg2023@gmail.com>
2024-01-08 13:16:57 +0300

    fix style

--- my-app/src/component/header/header.component.tsx
+++ my-app/src/component/header/header.component.tsx
@@ -3,6 +3,7 @@ import SearchComponent from "./search/search.component";
 import UserComponent from "./user/user.component";
 import style from "./header.module.css";
 import TextComponent from "../common/text.component";
+import { Link } from "react-router-dom";
 
 const HeaderComponent = () => {
   return (
@@ -10,7 +11,9 @@ const HeaderComponent = () => {
       <div className={style.container}>
         <div className={style.wrap}>
           <div className={style.title}>
-            <TextComponent text={"Bookstore"} />
+            <Link to={"/"}>
+              <TextComponent text={"Bookstore"} />
+            </Link>
           </div>
           <SearchComponent />
           <UserComponent />
--- my-app/src/component/header/user/user.component.tsx
+++ my-app/src/component/header/user/user.component.tsx
@@ -2,17 +2,24 @@ import React from "react";
 import FavoriteComponent from "./favorite/favorite.component";
 import CartComponent from "./cart/cart.component";
 import SignInUpComponent from "./sign-in-up/sign-in-up.component";
-import style from "./user.module.css"
+import style from "./user.module.css";
+import { Link } from "react-router-dom";
 
 const UserComponent = () => {
   return (
-    <nav className={style.container}>
-      <ul className={style.content}>
-        <li className={style.menuItem}><FavoriteComponent/></li>
-        <li className={style.menuItem}><CartComponent/></li>
-        <li className={style.menuItem}><SignInUpComponent/></li>
-      </ul>
-    </nav>
+    <div className={style.container}>
+      <div className={style.wrap}>
+        <Link to={"/bookmarks"}>
+          <FavoriteComponent />
+        </Link>
+        <Link to={"/cart"}>
+          <CartComponent />
+        </Link>
+        <Link to={"/sign-in"}>
+          <SignInUpComponent />
+        </Link>
+      </div>
+    </div>
   );
 };
 
