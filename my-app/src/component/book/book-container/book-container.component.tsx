9c261e831d2f8da33fec6e6f4a40e2a0bcfcc50b
Vitali Akulau <vitalibg2023@gmail.com>
2024-01-09 12:10:58 +0300

    add routing for books

--- my-app/src/component/book/book-container/book-container.component.tsx
+++ my-app/src/component/book/book-container/book-container.component.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import style from "./book-container.module.css";
 import BookComponent from "../book.component";
 import { IBook } from "../../../util/schema/new";
+import { Link } from "react-router-dom";
 
 interface BookContainerComponentProps {
   books: IBook[];
@@ -10,8 +11,10 @@ interface BookContainerComponentProps {
 const BookContainerComponent = ({ books }: BookContainerComponentProps) => {
   return (
     <div className={style.container}>
-      {books.map((book,i) => (
-        <BookComponent key={i} image={book.image} title={book.title} description={book.subtitle} price={book.price} />
+      {books.map((book, i) => (
+        <Link to={`/book-view/${i}`}>
+          <BookComponent key={i} image={book.image} title={book.title} description={book.subtitle} price={book.price} />
+        </Link>
       ))}
     </div>
   );
